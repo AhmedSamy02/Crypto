7d9f561b6438503f8d8b41f69577676a4959487c
AhmedSamy02 <https://github.com/AhmedSamy02>
2024-04-28 23:43:54 +0300

    [➕ FR] Updating AES to use SHA-256 for hashing key generator

--- chat_application/src/AES.java
+++ chat_application/src/AES.java
@@ -1,72 +1,66 @@
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.KeySpec;
+import java.util.Arrays;
 import java.util.Base64;
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 public class AES {
-    /* Private variable declaration */
-    private static final String SECRET_KEY = "123456789";
-    private static final String SALTVALUE = "abcdefg";
 
-    /* Encryption Method */
-    public static String encrypt(String strToEncrypt) {
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
+
+    public static void setKey(final String myKey) {
+        MessageDigest sha = null;
         try {
-            /* Declare a byte array. */
-            byte[] iv = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            IvParameterSpec ivspec = new IvParameterSpec(iv);
-            /* Create factory for secret keys. */
-            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-            /* PBEKeySpec class implements KeySpec interface. */
-            KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALTVALUE.getBytes(), 65536, 256);
-            SecretKey tmp = factory.generateSecret(spec);
-            SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
-            /* Retruns encrypted value. */
+            key = myKey.getBytes("UTF-8");
+            sha = MessageDigest.getInstance("SHA-256");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 32);
+            secretKey = new SecretKeySpec(key, "AES");
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String encrypt(final String strToEncrypt, final String secret) {
+        try {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
             return Base64.getEncoder()
-                    .encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
-        } catch (InvalidAlgorithmParameterException | InvalidKeyException | NoSuchAlgorithmException
-                | InvalidKeySpecException | BadPaddingException | IllegalBlockSizeException
-                | NoSuchPaddingException e) {
-            System.out.println("Error occured during encryption: " + e.toString());
+                    .encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+        } catch (Exception e) {
+            System.out.println("Error while encrypting: " + e.toString());
         }
         return null;
     }
 
-    /* Decryption Method */
-    public static String decrypt(String strToDecrypt) {
+    public static String decrypt(final String strToDecrypt, final String secret) {
         try {
-            /* Declare a byte array. */
-            byte[] iv = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            IvParameterSpec ivspec = new IvParameterSpec(iv);
-            /* Create factory for secret keys. */
-            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-            /* PBEKeySpec class implements KeySpec interface. */
-            KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALTVALUE.getBytes(), 65536, 256);
-            SecretKey tmp = factory.generateSecret(spec);
-            SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
-            /* Retruns decrypted value. */
-            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
-        } catch (InvalidAlgorithmParameterException | InvalidKeyException | NoSuchAlgorithmException
-                | InvalidKeySpecException | BadPaddingException | IllegalBlockSizeException
-                | NoSuchPaddingException e) {
-            System.out.println("Error occured during decryption: " + e.toString());
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(Base64.getDecoder()
+                    .decode(strToDecrypt)));
+        } catch (Exception e) {
+            System.out.println("Error while decrypting: " + e.toString());
         }
         return null;
     }
 
+    public static void main(final String[] args) {
+        final String secretKey = "ssshhhhhhhhhhh!!!!";
+
+        String originalString = "howtodoinjava.com";
+        String encryptedString = AES.encrypt(originalString, secretKey);
+        String decryptedString = AES.decrypt(encryptedString, secretKey);
+
+        System.out.println(originalString);
+        System.out.println(encryptedString);
+        System.out.println(decryptedString);
+    }
 }
